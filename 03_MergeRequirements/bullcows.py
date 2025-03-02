aaf68fbd8e93b8af052e57e61246fb2ea624a5c8
dvarfe <skt3032@ya.ru>
2025-03-02 14:49:05 +0300

    add reference implementation

--- 03_MergeRequirements/bullcows.py
+++ 03_MergeRequirements/bullcows.py
@@ -1,6 +1,9 @@
-from typing import Tuple, List
+from typing import Tuple, List, Optional
 from collections import Counter
+import sys
+import os
 from random import choice
+from urllib.request import urlopenn
 
 
 def bullcows(guess: str, ground_truth: str) -> Tuple[int, int]:
@@ -47,3 +50,22 @@ def gameplay(ask: callable, inform: callable, words: List[str]) -> int:
         bulls, cows = bullcows(guess, gt_word)
         inform("Быки: {}, Коровы: {}", bulls, cows)
     print(attempts)
+
+
+if __name__ == '__main__':
+    words_path, length = sys.argv[1], int(sys.argv[2])
+    words = ''
+    if os.path.isfile(words_path):
+        with open(words_path) as f:
+            words = f.readlines()
+        words = [word.strip() for word in words]
+    else:
+        data = urlopen(
+            'https://raw.githubusercontent.com/Harrix/Russian-Nouns/main/dist/russian_nouns.txt')
+        encoding = data.headers.get_param('charset')
+        if encoding is not None:
+            words = data.read().decode(encoding).split()
+        else:
+            words = data.read().decode('utf-8').split()
+    words = [word for word in words if len(word) == length]
+    gameplay(ask, inform, words)
