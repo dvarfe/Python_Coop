5d5b808b8b5073ff0f2d1f507258b0dfef71e8ef
dvarfe <skt3032@ya.ru>
2025-03-09 14:59:23 +0300

    create make_bubble command

--- 04_MergetoolCommandline/twocows.py
+++ 04_MergetoolCommandline/twocows.py
@@ -1,4 +1,4 @@
-from cowsay import cowsay, list_cows
+from cowsay import list_cows, make_bubble, THOUGHT_OPTIONS, Bubble
 import os
 import shlex
 import cmd
@@ -6,6 +6,9 @@ import cmd
 
 class twocows(cmd.Cmd):
     prompt = 'moo>> '
+
+    def do_EOF(self, args):
+        return 1
 
     def do_list_cows(self, arg):
         '''
@@ -20,6 +23,37 @@ class twocows(cmd.Cmd):
                 print('\n'.join(list_cows(path)))
         if len(arg) == 0:
             print('\n'.join(list_cows()))
+
+    def do_make_bubble(self, args):
+        '''
+        Makes text bubble. Has 4 arguments.
+        text
+        brackets=cowsay, -b - defines brackets to wrap text. Accepts 'cowsay' or 'cowthink'
+        width=40, -w
+        wrap-text=True, -r - whether wrap text or not. 
+        '''
+        args_parsed = shlex.split(args)
+        if len(args_parsed) == 0 or args_parsed[0][0] == '-':
+            print('No text specified!')
+            return
+
+        text = args_parsed[0]
+        brackets = THOUGHT_OPTIONS['cowsay']
+        width = 40
+        wrap_text = True
+        argit = iter(args_parsed)
+        next(argit)
+
+        for arg in argit:
+            match arg:
+                case '-b' | '--brackets':  brackets = THOUGHT_OPTIONS[next(argit).lower()]
+                case '-w' | '--width':  width = int(next(argit))
+                case '-r' | '--wrap-text':
+                    wrap_str = next(argit)
+                    wrap_text = bool(wrap_str.lower() ==
+                                     'true' or wrap_str == '1')
+
+            print(make_bubble(text, brackets, width, wrap_text))
 
 
 if __name__ == '__main__':
