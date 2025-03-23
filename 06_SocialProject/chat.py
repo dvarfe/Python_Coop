e02376c3259ddc1a2ebded3b3d1d046879b64c4e
dvarfe <skt3032@ya.ru>
2025-03-23 10:56:36 +0300

    modify server

--- 06_SocialProject/chat.py
+++ 06_SocialProject/chat.py
@@ -4,6 +4,7 @@ from cowsay import list_cows, cowsay
 clients = {}
 clients_names = {}
 cows_list = list_cows()
+command_num = {}
 
 
 async def chat(reader, writer):
@@ -11,6 +12,7 @@ async def chat(reader, writer):
     print(f'{me} has connected!', flush=True)
     my_name = ''
     quit_flag = 0
+    command_num[me] = 0
     clients[me] = asyncio.Queue()
     send = asyncio.create_task(reader.readline())
     receive = asyncio.create_task(clients[me].get())
@@ -24,20 +26,21 @@ async def chat(reader, writer):
                 match my_msg[0]:
                     case 'who':
                         if len(clients_names) == 0:
-                            await clients[me].put(f"You're the only cow")
+                            await clients[me].put(f"0:You're the only cow")
                         else:
-                            await clients[me].put(f'{'\n'.join(clients_names.keys())}')
+                            await clients[me].put(f'0:{'\n'.join(clients_names.keys())}')
                     case 'cows':
                         cows_avail = set(cows_list).difference(
                             set(clients_names.keys()))
-                        await clients[me].put(f'{'\n'.join(cows_avail)}')
+                        command_num[me] += 1
+                        await clients[me].put(f'{command_num[me]}:{'\n'.join(cows_avail)}')
                     case 'quit':
                         quit_flag = 1
                         if my_name != '':
                             clients_names.pop(my_name, None)
                     case 'login':
                         if my_name != '':
-                            await clients[me].put(f'You are already a cow!')
+                            await clients[me].put(f'0:You are already a cow!')
                         else:
                             login = my_msg[1]
                             cows_avail = set(cows_list).difference(
@@ -45,30 +48,30 @@ async def chat(reader, writer):
                             if login in cows_avail:
                                 my_name = login
                                 clients_names[my_name] = clients[me]
-                                await clients[me].put(f"Hello, {my_name}")
+                                await clients[me].put(f"0:Hello, {my_name}")
                             else:
-                                await clients[me].put(f"Please enter valid login!")
+                                await clients[me].put(f"0:Please enter valid login!")
                     case 'say':
                         receiver = my_msg[1]
                         if my_name == '':
-                            await clients[me].put('Login first!')
+                            await clients[me].put('0:Login first!')
                         elif (receiver not in list(clients_names.keys())) or (receiver == my_name):
-                            await clients[me].put(f"Unknown receiver!")
+                            await clients[me].put(f"0:Unknown receiver!")
                         else:
                             cow = cowsay(' '.join(my_msg[2:]),
                                          cow=my_name)
                             await clients_names[receiver].put(cow)
                     case 'yield':
                         if my_name == '':
-                            await clients[me].put(f"Login first!")
+                            await clients[me].put(f"0:Login first!")
                         else:
                             for out in clients.values():
                                 if out is not clients[me]:
                                     cow = cowsay(' '.join(my_msg[1:]),
                                                  cow=my_name)
-                                    await out.put(cow)
+                                    await out.put('0:'+cow)
                     case _:
-                        await clients[me].put(f"Invalid command!")
+                        await clients[me].put(f"0:Invalid command!")
             elif q is receive:
                 receive = asyncio.create_task(clients[me].get())
                 writer.write(f"{q.result()}\n".encode())
--- 06_SocialProject/client.py
+++ 06_SocialProject/client.py
@@ -3,21 +3,45 @@ import socket
 import cmd
 import threading
 import readline
+import time
+
+# message structure: [num]:[text]
 
 
-def get_msgs(s):
-    while msg := sys.stdin.buffer.readline():
-        try:
-            s.sendall(msg)
-        except ConnectionError:
-            return
-        print(f'\n{s.recv(1024).rstrip().decode()}\n')
+def get_msgs(cmd, s):
+    while msg := s.recv(1024):
+        print(f'\n{msg.rstrip().decode()}')
+        print(f'{cmd.prompt}{readline.get_line_buffer()}', end='', flush=True)
+
+
+class Client(cmd.Cmd):
+    prompt = 'moo>>'
+
+    def __init__(self, s):
+        self.s = s
+        super().__init__()
+        print(self.s)
+
+    def __send_command(self, command, args):
+        self.s.sendall((command + ' ' + args + '\n').encode())
+
+    def do_quit(self, args):
+        self.__send_command('quit', args)
+        return 1
+
+    def do_yield(self, args):
+        self.__send_command('yield', args)
+
+    def do_login(self, args):
+        self.__send_command('login', args)
 
 
 host = "localhost" if len(sys.argv) < 2 else sys.argv[1]
 port = 1337 if len(sys.argv) < 3 else int(sys.argv[2])
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((host, port))
-    server_thread = threading.Thread(target=get_msgs, args=(s,))
+    cmdline = Client(s)
+    server_thread = threading.Thread(target=get_msgs, args=(cmdline, s))
     server_thread.start()
+    cmdline.cmdloop()
     server_thread.join()
